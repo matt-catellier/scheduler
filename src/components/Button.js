73e39b0b75e48b063dcf73944d045ee7efd4da6e
Matt Catellier <mattcatellier@gmail.com>
2019-08-14 10:06:44 -0700

    Added all stories for Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,24 @@
 import React from "react";
+import classNames from "classnames";
 
 import "components/Button.scss";
 
-export default function Button(props) {
-   return <></>;
+export default function Button({
+  confirm,
+  danger,
+  onClick,
+  disabled,
+  children
+}) {
+  const buttonClass = classNames({
+    button: true,
+    "button--confirm": confirm,
+    "button--danger": danger
+  });
+
+  return (
+    <button disabled={disabled} className={buttonClass} onClick={onClick}>
+      {children}
+    </button>
+  );
 }
